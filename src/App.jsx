a1df1dad63d2e7726265fef79e3e6cde9bec1394
Natalia <sh.nat.vlad@gmail.com>
2024-05-03 12:51:56 +0500

    feat: add header and UserInput component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,30 @@
+import { useState } from "react";
+
+import Results from "./components/Results"
+import UserInput from "./components/UserInput"
+
 function App() {
+  const [ userInput, setUserInput ] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10
+  });
+
+  function handleChange(inputIndentifier, newValue) {
+    setUserInput((prevUserInput) => {
+        return {
+            ...prevUserInput,
+            [inputIndentifier]: newValue
+        }
+    })
+}
+
   return (
-    <h1>React Investment Calculator</h1>
+    <>
+      <UserInput onChangeInput={handleChange} userInput={userInput}/>
+      <Results />
+    </>    
   )
 }
 
